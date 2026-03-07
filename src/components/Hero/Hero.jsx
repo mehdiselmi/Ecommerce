edb6260fb011113fa7e634dcbf82f67b0c33de60
Mehdi <selmimehdi970@gmail.com>
2026-03-07 13:05:37 +0100

    resolve merge conflict in Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,21 +2,17 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className={`mt-25 md:mt-30 `}>
-<<<<<<< HEAD
-   
-      <div className=" bg-[url('/Frame.png')] bg-cover bg-center h-100    flex flex-col  justify-center items-center">
-        <h1 className="text-4xl md:text-5xl  font-bold ">
-=======
-      <div className=" bg-[url('Frame.png')] bg-cover bg-center h-100    flex flex-col  justify-center items-center">
-        <h1 className="text-3xl md:text-5xl   font-bold ">
->>>>>>> ae3f6e6 (Make website responsive)
-          Dress like you mean it{" "}
+    <div className="mt-25 md:mt-30">
+      <div className="bg-[url('/Frame.png')] bg-cover bg-center  h-screen flex flex-col justify-center items-center">
+        <h1 className="text-4xl md:text-5xl font-bold">
+          Dress like you mean it
         </h1>
-        <p className="md:px-100  py-5 md:py-3 text-primary">
+        <p className="md:px-20 py-5 md:py-3 text-primary">
           Sustainable Fashion
         </p>
-        <p className="text-secondary">Where Style Meets Comfort & Conscience</p>
+        <p className="text-secondary">
+          Where Style Meets Comfort & Conscience
+        </p>
       </div>
     </div>
   );
