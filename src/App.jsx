7a289d0da9c7d35a115a3bdf7c803fb973f7bc49
Mehdi <selmimehdi970@gmail.com>
2026-03-07 13:14:53 +0100

    resolve merge conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,19 @@
-
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Collection from "./components/Collection/Collection";
 import Feature from "./components/Feature/Feature";
 import About from "./components/About/About";
-const App = () => {
-  return (
-<<<<<<< HEAD
-    <div
-      className={dark ? "text-white bg-secondary " : "text-secondary bg-white"}
-    >
-      <Navbar dark={dark} />
- {/* <div className="flex justify-center  ">
-        <button onClick={() => setDark(!dark)}>
-          {dark ? (
-            <MdLightMode className="hover:scale-110 text-xl cursor-pointer transition-all duration-500" />
-          ) : (
-            <MdDarkMode className="hover:scale-110 text-xl cursor-pointer transition-all duration-500" />
-          )}
-        </button>
-      </div> */}
-      <Hero dark={dark} />
-     
-      <Collection dark={dark} />
-=======
-    <div>
-      {/* Navbar */}
-      <Navbar/>
->>>>>>> ae3f6e6 (Make website responsive)
 
-      <Hero />
-      <Collection />
-      <Feature />
-      <About />
+const App = () => {
+  const dark = false; 
+
+  return (
+    <div className={dark ? "text-white bg-secondary" : "text-secondary bg-white"}>
+      <Navbar dark={dark} />
+      <Hero dark={dark} />
+      <Collection dark={dark} />
+      <Feature dark={dark} />
+      <About dark={dark} />
     </div>
   );
 };
