110801109712d96ea1d94bac1974045c311c1737
Mehdi <selmimehdi970@gmail.com>
2026-03-14 14:13:17 +0100

    udpate folder app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import Navbar from "./components/Navbar/Navbar";
 
-
-import { Routes, BrowserRouter, Route } from "react-router-dom";
+import { Routes, HashRouter, Route } from "react-router-dom";
 import Shop from "./pages/Shop";
 import Product from "./pages/Products";
 import ShopCategory from "./pages/ShopCategory";
 import Cart from "./pages/Cart";
 import LoginSignUp from "./pages/LoginSignUp";
-import Contact from "./components/Contact/Contact";
+
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />}></Route>
@@ -21,15 +20,14 @@ const App = () => {
             element={<ShopCategory category="womens" />}
           ></Route>
           <Route path="/kids" element={<ShopCategory category="kid" />}></Route>
-          <Route path="/product" element={<Product />}></Route>
-          <Route path=":productId" element={<Product />}></Route>
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
 
           <Route path="/cart" element={<Cart />}></Route>
           <Route path="/login" element={<LoginSignUp />}></Route>
         </Routes>
-       
-      
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 };
